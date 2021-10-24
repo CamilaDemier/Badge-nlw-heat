8525567b96e6cb3eb29aab01fc301ec85a7929ed
CamilaDemier <71983963+CamilaDemier@users.noreply.github.com>
2021-10-24 17:11:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  youtube: 'UC6TdkDF8S4k7a8jhe4mDj_A',
+  facebook: 'camila.demier.3',
+  instagram: 'camilademierm',
+  twitter: 'lula',
+  github: 'CamilaDemier'
+}
+
+function changeSocialMediaLinks() {
+  //document.getElementById('userName').textContent = 'Ivana' -> faz a mesma coisa que -> userName.textContent = 'Maria'
+  for (let li of socialLinks.children) {
+    //i++ -> i=i+1
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
